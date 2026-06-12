d876986fd44c2cffe70a14a5e3996c0dc9c2d344
saurav7747 <izsaurav777@gmail.com>
2026-06-12 10:26:07 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// service-worker.js
+const CACHE_NAME = 'sauravscore-v1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
